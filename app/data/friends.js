27b2952b143374b86fce4f15d3cd15a1a88e4271
Alyssa Szutz <aaszutz@gmail.com>
2019-04-23 21:18:01 -0400

    working push

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+	{
+		name: "Penny",
+		photo: "https://vignette.wikia.nocookie.net/bigbangtheory/images/1/12/10.20_TRD-13.jpg/revision/latest/scale-to-width-down/220?cb=20170328200956",
+		scores: [1, 5, 1, 2, 5, 1, 1, 5, 2, 5] //28
+	},
+	{
+		name: "Lenoard",
+		photo: "https://i2.wp.com/metro.co.uk/wp-content/uploads/2018/11/gettyimages-938213170.jpg?quality=90&strip=all&zoom=1&resize=644%2C429&ssl=1",
+		scores: [4, 1, 1, 2, 1, 2, 4, 1, 5, 1] //22
+	},
+	{
+		name: "Raj",
+		photo: "https://upload.wikimedia.org/wikipedia/en/thumb/a/a1/Rajesh_Koothrappali.jpg/220px-Rajesh_Koothrappali.jpg",
+		scores: [4, 1, 5, 5, 5, 5, 3, 1, 1, 1] //31
+	},
+	{
+		name: "Sheldon",
+		photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_t33-KeuNn70MDfM0EoTxhODfv-h28dPe_inxamAVtHSiLBXAHA",
+		scores: [5, 1, 3, 2, 5, 2, 5, 1, 5, 1] //30
+	},
+	{
+		name: "Howard",
+		photo: "http://www.tvstyleguide.com/wp-content/uploads/2015/01/howard_wolowitz_blue_pants.jpg",
+		scores: [5, 4, 5, 5, 5, 1, 3, 1, 5, 1] //35
+	}
+];
+
+module.exports = friends;
